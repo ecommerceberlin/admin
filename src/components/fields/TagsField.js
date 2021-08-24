baeff7930a3c49a50d627d62971c859f3272cbfc
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-08-24 18:28:20 +0200

    ticket role, ticket tags - fix styling issues

--- src/components/fields/TagsField.js
+++ src/components/fields/TagsField.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Chip from '@material-ui/core/Chip';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+      marginRight: 5
+  }
+}));
 
 const TagsField = ({ record }) => {
+
+  const classes = useStyles()
 
   if(!record || !("tags" in record)){
     return null
@@ -10,7 +20,7 @@ const TagsField = ({ record }) => {
   return (
     <span>
       {record.tags.map(item => (
-        <Chip size="small" variant="outlined" key={item.name} label={item.name} />
+        <Chip size="small" variant="outlined" key={item.name} label={item.name} className={classes.root} />
       ))}
     </span>
   );
--- src/endpoints/tickets/fields/TicketRoleField.js
+++ src/endpoints/tickets/fields/TicketRoleField.js
@@ -11,6 +11,9 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+
+const importantRoles = ["exhibitor"]
+
 const TicketRoleField = ({record, labelNotSet}) => {
 
     const classes = useStyles()
@@ -22,7 +25,7 @@ const TicketRoleField = ({record, labelNotSet}) => {
     const value = (get(record, "role") || "").trim()
 
     if(roles && Array.isArray(roles) && roles.includes(value)){
-        return (<Chip color="secondary" label={value} size="small" className={classes.root} />)
+        return (<Chip color={importantRoles.includes(value)? "secondary": "default"} label={value} size="small" variant="outline" className={classes.root} />)
     }
 
     return labelNotSet ? <span>{labelNotSet}</span>: null
