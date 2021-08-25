f9b25f15d3a8912c8ecfadb08a7abe3ad013014a
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-08-25 23:45:21 +0200

    added sentry monitoring
    https://github.com/ecommerceberlin/admin/issues/54

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+Sentry.init({
+    dsn: "https://13f813306a6f4d94bfb34621ad447316@o14188.ingest.sentry.io/5931036",
+    integrations: [new Integrations.BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+  
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
