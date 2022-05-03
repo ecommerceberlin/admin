5da7a817f5e02ddd0bfdbe32a1badbeb3771e5e2
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-03 17:50:54 +0200

    Update ParticipantAside.js

--- src/endpoints/participants/components/ParticipantAside.js
+++ src/endpoints/participants/components/ParticipantAside.js
@@ -1,13 +1,17 @@
 import { useEffect } from 'react'
 import { Aside } from '../../../components'
-import { useGet } from '../../../api'
+import { useApiContext, useGet } from '../../../api'
 
 const ParticipantsAside = () => {
 
+    const [_, event_id] = useApiContext()
+    const {data, checking, error} = useGet(`participant-stats?event_id=${event_id}`)
+    const {not_going, going} = data
 
-    const {data, checking, error} = useGet("participant-stats")
-
-    return <Aside>asd</Aside>
+    return (<Aside>
+       Going {going} <br/>
+       Not going {not_going}
+    </Aside>)
 
 }
 
