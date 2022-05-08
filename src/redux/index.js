0a00e44feed21e8b4e9032c7ab11b47a1d0c6122
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-08 22:28:35 +0200

    remove redux as it is obsolete

--- src/redux/index.js
+++
@@ -1,4 +0,0 @@
-export { default as customReducers } from './reducers';
-export { default as customSagas } from './sagas';
-
-export * from './actions';
--- src/redux/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { default as app } from './app';
-import { default as ui } from './ui';
-
-export default { app, ui };
