ec72a9ba89f31e516f808746d1cfde47fb7fcb5f
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-28 16:47:48 +0200

    Delete customRoutes.js

--- src/customRoutes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-export default [
-  // <Route exact path="/foo" component={Foo} />,
-  // <Route exact path="/bar" component={Bar} />,
-  // <Route exact path="/baz" component={Baz} noLayout />,
-];
