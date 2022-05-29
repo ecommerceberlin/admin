86088804e9cb221f25293d0eaee4e2ce43443b68
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-29 13:18:13 +0200

    handle additional decoration on select $15$

--- src/components/MySelect.js
+++ src/components/MySelect.js
@@ -4,15 +4,13 @@ import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
+import Box from '@mui/material/Box';
+import { isFunction } from 'lodash';
  
 
-export default function SelectLabels({label, value, onChange, options}) {
+export default function SelectLabels({label, descriptionLabel, value, onChange, options, decorate, valueSource="id", labelSource="name"}) {
 
-
-    return (
-   
-    <FormControl sx={{ m: 1, minWidth: 120 }}>
+    return (<FormControl sx={{ m: 1, width: 250 }}>
     <InputLabel id="demo-simple-select-helper-label">{label}</InputLabel>
     <Select
     labelId="demo-simple-select-helper-label"
@@ -20,50 +18,20 @@ export default function SelectLabels({label, value, onChange, options}) {
     value={value}
     onChange={(e) => onChange(e.target.value)}
     label={label}
+    fullWidth
     >
     <MenuItem value="">
     <em>None</em>
     </MenuItem>
-    {(options || []).map(({id, name}) =>  <MenuItem key={id} value={id}>{name}</MenuItem>)}
+    {(options || []).map((option) => (<MenuItem key={option[valueSource]} value={option[valueSource]}>
+        <Box sx={{display: "flex", alignItems: "center"}}>
+        {isFunction(decorate)? decorate(option): null}
+        {option[labelSource]}
+        </Box>
+        </MenuItem>))}
     </Select>
-    <FormHelperText>{label}</FormHelperText>
-    </FormControl>
-
-    );
+    {descriptionLabel? <FormHelperText>{descriptionLabel}</FormHelperText>: null}
+    </FormControl>);
 }
 
 
-
-/**
- * 
- * 
- 
-
-
-
-
-    const handleChangeEvent = (e) => {
-         (changeEvent(find(data, {id: e.target.value})))
-         (hideDialog());
-    }
-
-    const filteredData = (data || []).filter(event => event.group_id == group_id)
-
-    return (
-        <FormControl className={classes.formControl}>
-        <InputLabel id="change-event-label">Event</InputLabel>
-        <Select
-            id="select-event"
-            labelId="change-event-label"
-            value={event_id}
-            onChange={ handleChangeEvent }
-            autoWidth={true}
-            variant="outlined"
-            className={classes.select}
-        >
-        {filteredData.map(({id, name, is_active}) => <MenuItem key={id} value={id}>{is_active && <ActiveIcon className={classes.icon}/>}{name}</MenuItem> )}
-        </Select>
-        </FormControl>)
-
-
- */
